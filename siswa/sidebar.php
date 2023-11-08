e5921f7b319085dae15197ef000b00dfa19b2926
Arsyad Ghani <139409989+ailobluu@users.noreply.github.com>
2023-11-08 12:35:20 +0700

    Add files via upload

---
+++ siswa/sidebar.php
@@ -0,0 +1,25 @@
+            <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
+                <div class="sb-sidenav-menu">
+                    <div class="nav">
+                        <div class="sb-sidenav-menu-heading">Menu Utama</div>
+                        <a class="nav-link" href="index.php">
+                            <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                            Dashboard
+                        </a>
+                        <a class="nav-link" href="anggota">
+                            <div class="sb-nav-link-icon"><i class="fas fa-paste"></i></div>
+                            Data Anggota
+                        </a>
+                        <a class="nav-link" href="buku">
+                            <div class="sb-nav-link-icon"><i class="fas fa-paste"></i></div>
+                            Buku
+                        </a>
+                        <a class="nav-link" href="peminjaman">
+                            <div class="sb-nav-link-icon"><i class="fas fa-paste"></i></div>
+                            Peminjaman
+                        </a>
+                        <a class="nav-link" href="petugas">
+                            <div class="sb-nav-link-icon"><i class="fas fa-paste"></i></div>
+                            Petugas
+                        </a>
+            </nav>
